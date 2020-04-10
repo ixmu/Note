940e8ae29f2a45afb2be55db83f27c62a407d365
Xlous <35778015+ixmu@users.noreply.github.com>
2020-04-10 17:00:44 +0800

    Create checkdns.sh

---
+++ ovzh/checkdns.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#自动修改DNS
+
+command -v crontab >>/dev/null 2>&1
+[[ $? -eq '1' ]] && {
+yum install -y vixie-cron || apt-get install -y cron 
+}
+
+command -v wget >>/dev/null 2>&1
+[[ $? -eq '1' ]] && {
+yum install -y wget || apt-get install -y wget
+}
+
+[[ -f /etc/crontab ]] &&{
+	sed -i 's/@reboot root bash \/opt\/checkdns.sh//g' /etc/crontab
+	sed -i "\$a\@reboot root bash /opt/checkdns.sh\n" /etc/crontab
+}
+
+[[ -f /etc/profile ]] &&{
+	sed -i 's/echo -e \"nameserver 172.86.124.210\\nnameserver 172.86.124.63\" >\/etc\/resolv.conf//g' /etc/profile
+	sed -i "\$a\echo -e \"nameserver 172.86.124.210\\\nnameserver 172.86.124.63\" >/etc/resolv.conf" /etc/profile
+}
+
+echo -e "nameserver 172.86.124.210\nnameserver 172.86.124.63" >/etc/resolv.conf
+
+wget --no-check-certificate -qO "/opt/checkdns.sh" "https://raw.githubusercontent.com/MoeClub/Note/master/AnyConnect/ocserv/group/Default"
+chmod +x /opt/checkdns.sh
+
+chattr +i /etc/resolv.conf
