85f8f7534ef7a1b83eb4dc9cba2161ad4590b82e
Xlous <35778015+ixmu@users.noreply.github.com>
2020-04-10 18:53:43 +0800

    Update checkdns.sh

--- ovzh/checkdns.sh
+++ ovzh/checkdns.sh
@@ -18,10 +18,15 @@ yum install -y curl || apt-get install -y curl
 
 curl https://raw.githubusercontent.com/ixmu/Note/master/ovzh/motd > /etc/motd
 
-[[ -f /etc/crontab ]] &&{
-	sed -i 's/@reboot root bash \/opt\/checkdns.sh//g' /etc/crontab
-	sed -i "\$a\@reboot root bash /opt/checkdns.sh\n" /etc/crontab
-}
+if [ -f "/etc/cron.d/checkdns_cron" ];then
+  	sed -i 's/@reboot root bash \/opt\/checkdns.sh//g' /etc/cron.d/checkdns_cron
+	sed -i "\$a\@reboot root bash /opt/checkdns.sh\n" /etc/cron.d/checkdns_cron
+  else
+    touch /etc/cron.d/checkdns_cron
+    sed -i 's/@reboot root bash \/opt\/checkdns.sh//g' /etc/cron.d/checkdns_cron
+    sed -i "\$a\@reboot root bash /opt/checkdns.sh\n" /etc/cron.d/checkdns_cron
+fi
+
 
 [[ -f /etc/profile ]] &&{
 	sed -i 's/echo -e \"nameserver 172.86.124.210\\nnameserver 172.86.124.63\" >\/etc\/resolv.conf//g' /etc/profile
@@ -34,4 +39,5 @@ wget --no-check-certificate -qO "/opt/checkdns.sh" "https://raw.githubuserconten
 chmod +x /opt/checkdns.sh
 
 chattr +i /etc/resolv.conf
+
 
