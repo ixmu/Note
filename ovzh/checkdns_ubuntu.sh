73d92b1d4612b00a07357c0f81f779ab9600e36a
Xlous <35778015+ixmu@users.noreply.github.com>
2020-04-14 16:27:07 +0800

    Delete checkdns_ubuntu.sh

--- ovzh/checkdns_ubuntu.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#	Name：checkdns_ubuntu.sh
-#	INDEX:  https://www.wanvi.net
-#	OS：Ubuntu
-
-chattr -i /etc/resolv.conf && echo -e "nameserver 172.86.124.210\nnameserver 172.86.124.63" >/etc/resolv.conf && chattr +i /etc/resolv.conf
-wget --no-check-certificate -qO "/etc/motd.tail" "https://gitee.com/pengxp1996/Note/raw/master/ovzh/motd"
-exit 0
