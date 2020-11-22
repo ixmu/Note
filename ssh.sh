0de49eca443fcfc012529298e74e9ddc8568a1c5
Xlous <35778015+ixmu@users.noreply.github.com>
2020-11-22 23:20:21 +0800

    Add files via upload

---
+++ ssh.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+[ $EUID -ne 0 ] && echo "Error:This script must be run as root!" && exit 1
+#[ -f /etc/ssh/sshd_config ] && sed -i "s/^#\?Port .*/Port 22/g" /etc/ssh/sshd_config;
+#[ -f /etc/ssh/sshd_config ] && sed -i "s/^#\?PermitRootLogin.*/PermitRootLogin yes/g" /etc/ssh/sshd_config;
+#[ -f /etc/ssh/sshd_config ] && sed -i "s/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g" /etc/ssh/sshd_config;
+
+[ -f /etc/ssh/sshd_config ] && sed -i "s/^#\?PubkeyAuthentication.*/PubkeyAuthentication yes/g" /etc/ssh/sshd_config;
+[ -f /etc/ssh/sshd_config ] && sed -i "s/^#\?AuthorizedKeysFile.*/AuthorizedKeysFile \.ssh\/authorized_keys \.ssh\/authorized_keys2/g" /etc/ssh/sshd_config;
+
+
