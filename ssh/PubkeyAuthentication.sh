fb81548fd0074852368d4bd6cb5e84ea68748ec3
xlouspeng <pengxp1996@hotmail.com>
2020-12-17 17:10:10 +0800

    Update PubkeyAuthentication.sh

--- ssh/PubkeyAuthentication.sh
+++ ssh/PubkeyAuthentication.sh
@@ -11,9 +11,9 @@ fi
 #检测用户
 [ $EUID -ne 0 ] && echo "Error:This script must be run as root!" && exit 1
 #设置ssh端口为22
-[ -f /etc/ssh/sshd_config ] && sed -i "s/^#\?Port .*/Port 22/g" /etc/ssh/sshd_config;
+#[ -f /etc/ssh/sshd_config ] && sed -i "s/^#\?Port .*/Port 22/g" /etc/ssh/sshd_config;
 #允许root登录
-[ -f /etc/ssh/sshd_config ] && sed -i "s/^#\?PermitRootLogin.*/PermitRootLogin yes/g" /etc/ssh/sshd_config;
+#[ -f /etc/ssh/sshd_config ] && sed -i "s/^#\?PermitRootLogin.*/PermitRootLogin yes/g" /etc/ssh/sshd_config;
 #禁止密码认证
 #[ -f /etc/ssh/sshd_config ] && sed -i "s/^#\?PasswordAuthentication.*/PasswordAuthentication no/g" /etc/ssh/sshd_config;
 #启用公钥认证
