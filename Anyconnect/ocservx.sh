006f5ea67637e06423cf2890007c40b5adf41508
xlouspeng <pengxp1996@hotmail.com>
2020-12-24 20:57:31 +0800

    Update ocservx.sh

--- Anyconnect/ocservx.sh
+++ Anyconnect/ocservx.sh
@@ -102,10 +102,10 @@ fi
 [ -f /etc/ocserv/ocserv.conf ] && sed -i "s/^#\?output-buffer.*/output-buffer \= 64/g" /etc/ocserv/ocserv.conf;
 [ -f /etc/ocserv/ocserv.conf ] && sed -i "s/max-clients.*/max-clients \= 256/g" /etc/ocserv/ocserv.conf;
 [ -f /etc/ocserv/ocserv.conf ] && sed -i "s/max-same-clients.*/max-same-clients \= 3/g" /etc/ocserv/ocserv.conf;
-wget --no-check-certificate -4 -qO /etc/ocserv/profile.xml 'https://cdn.jsdelivr.net/gh/ixmu/Note@master/Anyconnect/ocserv/profile.xml'
-wget --no-check-certificate -4 -qO /etc/ocserv/ca.cert.pem 'https://www.ixmu.net/project/ocserv/ca.cert.pem'
-wget --no-check-certificate -4 -qO /etc/ocserv/server.key.pem 'https://www.ixmu.net/project/ocserv/server.key.pem'
-wget --no-check-certificate -4 -qO /etc/ocserv/server.cert.pem 'https://www.ixmu.net/project/ocserv/server.cert.pem'
+wget --no-check-certificate -4 -qO /etc/ocserv/profile.xml 'https://raw.githubusercontent.com/ixmu/Note/master/Anyconnect/ocserv/profile.xml'
+wget --no-check-certificate -4 -qO /etc/ocserv/ca.cert.pem 'https://raw.githubusercontent.com/ixmu/Note/master/Anyconnect/ocserv/ca.cert.pem'
+wget --no-check-certificate -4 -qO /etc/ocserv/server.key.pem 'https://api.ixmu.net/server.key.pem'
+wget --no-check-certificate -4 -qO /etc/ocserv/server.cert.pem 'https://api.ixmu.net/server.cert.pem'
 
 # Dnsmasq Configuration
 [ -f /etc/ocserv/ocserv.conf ] && sed -i "s/192\.168\.8/192\.168\.7/g" /etc/ocserv/ocserv.conf;
