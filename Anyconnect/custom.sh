3f9e566c3634dc1f7749872981317524994ea84e
Xlous <35778015+ixmu@users.noreply.github.com>
2021-01-05 22:19:22 +0800

    Update custom.sh

--- Anyconnect/custom.sh
+++ Anyconnect/custom.sh
@@ -3,10 +3,10 @@
 # Custom Mmade In XlouusPeng
 
 #Ocserv Configuration
-[ -f /etc/ocserv/ocserv.conf ] && sed -i "s/^#\?user-profile.*/user-profile \= \/etc\/ocserv\/profile\.xml/g" /etc/ocserv/ocserv.conf;
-[ -f /etc/ocserv/ocserv.conf ] && sed -i "s/^#\?output-buffer.*/output-buffer \= 64/g" /etc/ocserv/ocserv.conf;
-[ -f /etc/ocserv/ocserv.conf ] && sed -i "s/max-clients.*/max-clients \= 256/g" /etc/ocserv/ocserv.conf;
-[ -f /etc/ocserv/ocserv.conf ] && sed -i "s/max-same-clients.*/max-same-clients \= 3/g" /etc/ocserv/ocserv.conf;
+#[ -f /etc/ocserv/ocserv.conf ] && sed -i "s/^#\?user-profile.*/user-profile \= \/etc\/ocserv\/profile\.xml/g" /etc/ocserv/ocserv.conf;
+#[ -f /etc/ocserv/ocserv.conf ] && sed -i "s/^#\?output-buffer.*/output-buffer \= 64/g" /etc/ocserv/ocserv.conf;
+#[ -f /etc/ocserv/ocserv.conf ] && sed -i "s/max-clients.*/max-clients \= 256/g" /etc/ocserv/ocserv.conf;
+#[ -f /etc/ocserv/ocserv.conf ] && sed -i "s/max-same-clients.*/max-same-clients \= 3/g" /etc/ocserv/ocserv.conf;
 wget --no-check-certificate -4 -qO /etc/ocserv/profile.xml 'https://raw.githubusercontent.com/ixmu/Note/master/Anyconnect/ocserv/profile.xml'
 wget --no-check-certificate -4 -qO /etc/ocserv/ca.cert.pem 'https://raw.githubusercontent.com/ixmu/Note/master/Anyconnect/ocserv/ca.cert.pem'
 wget --no-check-certificate -4 -qO /etc/ocserv/server.key.pem 'https://api.ixmu.net/server.key.pem'
