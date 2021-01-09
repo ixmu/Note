e3145b14b7934f7bd79e98d12b22f32773c5ce78
Xlous <35778015+ixmu@users.noreply.github.com>
2021-01-09 20:17:18 +0800

    Update NoRoute_build.py

--- Anyconnect/build/NoRoute_build.py
+++ Anyconnect/build/NoRoute_build.py
@@ -3,13 +3,7 @@
 import os,re,netaddr,requests
 from netaddr import *
 
-cnlines = [line.rstrip('\n') for line in open('cnroute.txt')]
-cnsummary = netaddr.cidr_merge(sorted(cnlines))
-cnroute_merged = open("cnroute_merged.txt", "w", encoding='utf-8')
-cnroute_merged.write('\n'.join([ unicode(x) for x in cnsummary ]))
-cnroute_merged.close()
-
-
+r = requests.get('https://raw.githubusercontent.com/CNMan/chinaroute/master/cnroute_merged.txt')
 cnroute_merged = open('cnroute_merged.txt', 'w')
 cnroute_merged.write(r.text)
 cnroute_merged.close()
