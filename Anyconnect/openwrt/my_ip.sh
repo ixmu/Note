70f09c55272038e9b0e8ead88ab003c39e8b3d74
Xlous <35778015+ixmu@users.noreply.github.com>
2022-08-19 08:06:37 +0800

    Update my_ip.sh

--- Anyconnect/openwrt/my_ip.sh
+++ Anyconnect/openwrt/my_ip.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 run_connect(){
 # 添加gfwtable到 /etc/iproute2/rt_tables
-  cat /etc/iproute2/rt_tables |grep gfwtable
-  if [ $? -ne 0 ];then
+  cat /etc/iproute2/rt_tables |grep gfwtable >>/dev/null 2>&1
+  if [ $? == 0 ];then
   echo '999     gfwtable' >> /etc/iproute2/rt_tables
   fi
 
 # 添加防火墙iptables规则
-  ip rule list |grep gfwtable
-  if [ $? -ne 0 ];then
-     ipset create gfwlist hash:net family inet timeout 86400 maxelem 130050
-     iptables -t mangle -N fwmark
-     iptables -t mangle -A OUTPUT -j fwmark
-     iptables -t mangle -A PREROUTING -j fwmark
-     iptables -t mangle -A fwmark -m set --match-set gfwlist dst -j MARK --set-mark 0xffff
-     ip rule add fwmark 0xffff table gfwtable
-     ip route add default dev vpn-iepl table gfwtable
+  ipset list | grep "Name: gfwlist" >/dev/null 2>&1
+  if [ $? == 0 ];then echo "存在，跳过";else echo "不存在，开始创建";ipset create gfwlist hash:net family inet timeout 86400 maxelem 130050;fi
+  iptables -t mangle --list |grep fwmark >/dev/null 2>&1
+  if [ $? == 0 ];then echo "存在，跳过";else echo "不存在，开始创建";iptables -t mangle -N fwmark;fi
+  iptables -t mangle -C OUTPUT -j fwmark >/dev/null 2>&1
+  if [ $? == 0 ];then echo "存在，跳过";else echo "不存在，开始创建";iptables -t mangle -A OUTPUT -j fwmark;fi
+  iptables -t mangle -C PREROUTING -j fwmark >/dev/null 2>&1
+  if [ $? == 0 ];then echo "存在，跳过";else echo "不存在，开始创建";iptables -t mangle -A PREROUTING -j fwmark;fi
+  iptables -t mangle --list |grep "match-set gfwlist" >/dev/null 2>&1
+  if [ $? == 0 ];then echo "存在，跳过";else echo "不存在，开始创建";iptables -t mangle -A fwmark -m set --match-set gfwlist dst -j MARK --set-mark 0xffff;fi
+  ip rule list |grep "gfwtable" >/dev/null 2>&1
+  if [ $? == 0 ];then echo "存在，跳过";else echo "不存在，开始创建";ip rule add fwmark 0xffff table gfwtable;fi
+  sleep 90s
+  ip route add default dev vpn-iepl table gfwtable
 
-  fi
+
 
 # dns 和 telegram ip 
   ipset add gfwlist 8.8.8.0/24
@@ -36,22 +41,19 @@ run_connect(){
 
 
 # 华为ddns更新
-vpn_iepl_ip="$(wget --no-check-certificate -4 -qO- http://checkip.amazonaws.com)"
-curl -sL "https://api.ixmu.net/HWDNS?key=JLXCJEKP2IO7BUXMMSHK&secret=OwAODpwtrJuaSbIJ9wsGamJBilH6FZNBDdzciCjJ&action=add&target=record&domain=ixmu.eu.org&name=openwrt&data=${vpn_iepl_ip}&type=A&ttl=15"
+vpn_iepl_ip=="$(wget --no-check-certificate -4 -qO- http://checkip.amazonaws.com)"
+curl -sL "https://api.ixmu.net/HWDNS?key==JLXCJEKP2IO7BUXMMSHK&secret==OwAODpwtrJuaSbIJ9wsGamJBilH6FZNBDdzciCjJ&action==add&target==record&domain==ixmu.eu.org&name==openwrt&data==${vpn_iepl_ip}&type==A&ttl==15"
 
 
 
 # 更新gfwlist
 
-  latest_ver_durl=`curl -sSL https://api.github.com/repos/Loyalsoldier/v2ray-rules-dat/releases/latest |grep gfw.txt | awk -F '"' '{print $4}' |grep https`
-  latest_ver_code=`echo ${latest_ver_durl} |awk -F '/' '{print substr($8,1,8)}'`
-  now_time_ver=`date +%Y%m%d`
+  latest_ver_durl==`curl -sSL https://api.github.com/repos/Loyalsoldier/v2ray-rules-dat/releases/latest |grep gfw.txt | awk -F '"' '{print $4}' |grep https`
+  latest_ver_code==`echo ${latest_ver_durl} |awk -F '/' '{print substr($8,1,8)}'`
+  now_time_ver==`date +%Y%m%d`
   if [ ${now_time_ver} == ${latest_ver_code} ];then
     curl -sSl ${latest_ver_durl} > /etc/mosdns/rule/gfw.txt
   fi
-
-# 心跳
-ping -c4 192.168.8.1 -Ivpn-iepl
 }
 
 run_disconnect(){
@@ -62,7 +64,9 @@ run_disconnect(){
   
 }
 
-run_mode=`echo $1`
+run_mode_default="connect"
+run_mode_wen1=`echo $?`
+run_mode=`echo ${run_mode_wen1:-$run_mode_default}`
 
 if [ ${run_mode} == connect ]
 then
@@ -70,6 +74,9 @@ then
 elif [ ${run_mode} == disconnect ]
 then
    run_disconnect
-else
-   echo "请选择模式 connect | disconnect"
+   else
+   run_connect
 fi
+
+# 心跳
+ping -c4 192.168.8.1 -Ivpn-iepl
