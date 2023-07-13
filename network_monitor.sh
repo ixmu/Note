f9ef66c6ea1ce31c6f1b8739bda8e64f64d1b5bb
Xlous <35778015+ixmu@users.noreply.github.com>
2023-07-13 16:16:59 +0800

    Create network_monitor.sh

---
+++ network_monitor.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+while true; do
+    if ping -c 1 google.com &> /dev/null; then
+        echo "网络连接正常"
+    else
+        echo "网络连接失败，执行重启操作..."
+        reboot
+    fi
+    sleep 5
+done
