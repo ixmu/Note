dbf2669a0d64cd9111ad1069605eb5abcdb1c1ab
Xlous <35778015+ixmu@users.noreply.github.com>
2023-07-15 21:41:30 +0800

    Update cc-custom.sh

--- Anyconnect/cc-custom.sh
+++ Anyconnect/cc-custom.sh
@@ -1,10 +1,19 @@
 apt install -y iptables
+
+#初始化安装脚本
 wget --no-check-certificate --no-cache -4 -O /tmp/ocserv.sh "https://raw.githubusercontent.com/MoeClub/Note/master/AnyConnect/ocserv.sh"
 sed -i -e :a -e '$d;N;2,3ba' -e 'P;D' /tmp/ocserv.sh
 bash /tmp/ocserv.sh 
+
+#注入用户名密码信息
 UserPasswd=`openssl passwd 1810813019`
 echo -e "1810813019:Default:${UserPasswd}" >>/etc/ocserv/ocpasswd
+
+#修改配置文件
 sed -i 's/max-clients = 0/max-clients = 64/g' /etc/ocserv/ocserv.conf
 sed -i 's/max-same-clients = 0/max-same-clients = 64/g' /etc/ocserv/ocserv.conf
-curl -sSL https://raw.githubusercontent.com/ixmu/Note/master/Anyconnect/ocserv/ca.cert.pem > /etc/ocserv/ca.cert.pem
+sed -i 's/#user-profile.*/user-profile = \/etc\/ocserv\/profile.xml/g' /etc/ocserv/ocserv.conf
+curl -sSL https://raw.githubusercontent.com/ixmu/Note/master/Anyconnect/ocserv/profile.xml > /etc/ocserv/profile.xml
+
+#重启系统
 reboot 
