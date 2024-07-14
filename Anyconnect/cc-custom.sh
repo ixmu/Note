b8390b971c63e34889feb1c4e9b32b4a8c2a733a
marx peng <35778015+ixmu@users.noreply.github.com>
2024-07-15 00:05:04 +0800

    Update cc-custom.sh

--- Anyconnect/cc-custom.sh
+++ Anyconnect/cc-custom.sh
@@ -5,7 +5,7 @@ apt install iptables -y
 #初始化安装脚本
 wget --no-check-certificate --no-cache -4 -O /tmp/ocserv.sh "https://raw.githubusercontent.com/MoeClub/Note/master/AnyConnect/ocserv.sh"
 sed -i -e :a -e '$d;N;2,3ba' -e 'P;D' /tmp/ocserv.sh
-sed -i 's/v1.1.6/v1.1.7/' /tmp/ocserv.sh
+sed -i 's/v1.1.6/v1.2.2/' /tmp/ocserv.sh
 bash /tmp/ocserv.sh 
 
 #注入用户名密码信息
