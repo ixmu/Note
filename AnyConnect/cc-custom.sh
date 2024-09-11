2a31d44139823dc91fdc9454e689187e80180269
marx peng <35778015+ixmu@users.noreply.github.com>
2024-09-11 22:37:15 +0800

    Update cc-custom.sh

--- AnyConnect/cc-custom.sh
+++ AnyConnect/cc-custom.sh
@@ -28,7 +28,7 @@ bash <(wget --no-check-certificate -4 -qO- 'https://raw.githubusercontent.com/Mo
 # vlmcs
 if [ "$VER" == "amd64" ]; then
   rm -rf /etc/vlmcs
-  wget --no-check-certificate -4 -qO /tmp/vlmcs.tar "https://raw.githubusercontent.com/MoeClub/Note/master/AnyConnect/vlmcsd/vlmcsd_${VER}.tar"
+  wget --no-check-certificate -4 -qO /tmp/vlmcs.tar.xz "https://raw.githubusercontent.com/MoeClub/Note/master/AnyConnect/vlmcsd/vlmcsd_${VER}.tar"
   tar --overwrite -xvf /tmp/vlmcs.tar -C /
   [ -f /etc/vlmcs/vlmcs.d ] && bash /etc/vlmcs/vlmcs.d init
 fi
@@ -49,7 +49,7 @@ fi
 
 # ocserv
 rm -rf /etc/ocserv
-wget --no-check-certificate -4 -qO /tmp/ocserv_bin.tar "https://github.com/robvanoostenrijk/openconnect-server-static/releases/download/v1.1.6/openconnect-server-linux-${VER}.tar.xz"
+wget --no-check-certificate -4 -qO /tmp/ocserv_bin.tar.xz "https://github.com/robvanoostenrijk/openconnect-server-static/releases/download/v1.1.6/openconnect-server-linux-${VER}.tar.xz"
 tar --overwrite -xvJf /tmp/ocserv_bin.tar.xz -C /
 wget --no-check-certificate -4 -qO /tmp/ocserv_config.tar "https://raw.githubusercontent.com/MoeClub/Note/master/AnyConnect/build/ocserv_config.tar"
 tar --overwrite -xvf /tmp/ocserv_config.tar -C /
