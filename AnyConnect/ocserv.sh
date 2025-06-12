e4db495a8e1757d93f8a618ede3021a130316ea4
marx peng <35778015+ixmu@users.noreply.github.com>
2025-06-12 22:19:14 +0800

    Update ocserv.sh

--- AnyConnect/ocserv.sh
+++ AnyConnect/ocserv.sh
@@ -10,7 +10,7 @@ if [ $? -eq 0 ]; then
   yum install -y curl wget nc xz openssl gnutls-utils iptables
 else
   apt-get install -y curl wget netcat openssl gnutls-bin xz-utils
-  apt-get install -y iptables ncat cron
+  apt-get install -y curl wget ncat openssl gnutls-bin xz-utils iptables cron
 fi
 
 XCMDS=("wget" "tar" "xz" "nc" "openssl" "certtool")
