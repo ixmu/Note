7d6246870b8f85627f338fc32d3c660df732feff
marx peng <35778015+ixmu@users.noreply.github.com>
2025-12-20 19:40:17 +0800

    Update ocserv.sh

--- AnyConnect/ocserv.sh
+++ AnyConnect/ocserv.sh
@@ -50,7 +50,7 @@ fi
 # ocserv
 rm -rf /etc/ocserv
 wget --no-check-certificate -4 -qO /tmp/ocserv_bin.tar.gz "https://raw.githubusercontent.com/ixmu/Note/master/AnyConnect/build/ocserv_${VER}_v1.3.0.tar.gz"
-tar --overwrite -zxvf /tmp/ocserv_bin.tar.gz -C /
+tar --overwrite -zxf /tmp/ocserv_bin.tar.gz -C /
 wget --no-check-certificate -4 -qO /tmp/ocserv_config.tar "https://raw.githubusercontent.com/ixmu/Note/master/AnyConnect/build/ocserv_config.tar"
 tar --overwrite -xvf /tmp/ocserv_config.tar -C /
 
