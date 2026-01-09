c68de95f4da53180e636cadadbbce50752d7cbc8
marx peng <35778015+ixmu@users.noreply.github.com>
2026-01-09 23:35:06 +0800

    Update ocserv_install.sh

--- Ocserv/ocserv_install.sh
+++ Ocserv/ocserv_install.sh
@@ -28,8 +28,8 @@ PublicIP="$(wget --no-check-certificate -4 -qO- http://checkip.amazonaws.com)"
 # vlmcs
 if [ "$Arch" == "amd64" ]; then
   rm -rf /etc/vlmcs
-  wget --no-check-certificate -4 -qO /tmp/vlmcs.tar "https://raw.githubusercontent.com/ixmu/Note/master/Ocserv/build/vlmcsd.tar"
-  tar --overwrite -xvf /tmp/vlmcs.tar -C /
+  wget --no-check-certificate -4 -qO /usr/sbin/vlmcsdmulti "https://raw.githubusercontent.com/ixmu/Note/refs/heads/master/Ocserv/build/vlmcsdmulti_${Arch}"
+  chmod +x /usr/sbin/vlmcsdmulti
   curl -o /etc/systemd/system/vlmcsd.service https://raw.githubusercontent.com/ixmu/Note/master/Ocserv/build/vlmcsd.service
   systemctl daemon-reload
   systemctl enable vlmcsd.service --now >>/dev/null 2>&1
