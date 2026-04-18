bf42920928c54c7d92135e3c96c5dfb047932ed0
marx peng <35778015+ixmu@users.noreply.github.com>
2026-04-18 18:19:46 +0800

    add

---
+++ Ocserv/build/client.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+command -v openssl >>/dev/null 2>&1
+[ $? -ne 0 ] && echo "Not Found 'openssl'" && exit 1
+cd `dirname "$0"`
+
+export OrgName
+export GroupName
+export PASSWORD
+export INIT
+export BLANK=" "
+
+
+while [[ $# -ge 1 ]]; do
+  case $1 in
+    -o)
+      shift
+      OrgName=`echo "$1" |sed 's/\s//g'`
+      shift
+      ;;
+    -g)
+      shift
+      GroupName=`echo "$1" |sed 's/\s//g'`
+      shift
+      ;;
+    -p)
+      shift
+      PASSWORD=`echo "$1" |sed 's/\s//g'`
+      shift
+      ;;
+    -i)
+      shift
+      INI_=`echo $1 |sed 's/\s//g'`
+      INIT="${INI_:-0.0.0.0}"
+      shift
+      ;;
+    *)
+      echo -e "Usage:\n\tbash $0 -o <OrgName> -g <GroupName> -p <PASSWORD> -i <CN>\n"
+      exit 1;
+      ;;
+  esac
+done
+
+
+[ -n "$INIT" ] && [ -f "./ca.cert.pem" ] && [ -n "${OrgName}" ] && rm -rf "./ca.cert.pem"
+[ "$BLANK" == " " ] && [ -f "$HOME/.rnd" ] || openssl rand -writerand "$HOME/.rnd"
+[ -f "./ca.cert.pem" ] && OrgName=`openssl x509 -noout -in "./ca.cert.pem" -subject 2>/dev/null |sed 's/.*\s*O\s\+=\s\+\([^,\ ]\+\),.*/\1/'`
+
+[ -n "${OrgName}" ] || OrgName="MoeClub"
+[ -n "${GroupName}" ] || GroupName="Default"
+
+
+if [ ! -f ./ca.cert.pem -o ! -f ./ca.key.pem ] || [ -n "$INIT" ] ; then
+  openssl req -x509 -sha256 -newkey ec -pkeyopt ec_paramgen_curve:prime256v1 -nodes -days 3650 -subj "/C=${BLANK}${BLANK}/ST=${BLANK}/L=${BLANK}/OU=${BLANK}/O=${OrgName}/CN=${OrgName} CA" -addext "keyUsage=critical, keyCertSign, cRLSign" -rand /dev/urandom -outform PEM -keyout "./ca.key.pem" -out "./ca.cert.pem"  >/dev/null 2>&1
+
+  [ $? -ne 0 ] && echo "Generating CA Fail" && exit 1
+  cp -rf ./ca.cert.pem ../ca.cert.pem
+ 
+  openssl req -x509 -sha256 -newkey ec -pkeyopt ec_paramgen_curve:prime256v1 -nodes -days 3650 -subj "/C=${BLANK}${BLANK}/ST=${BLANK}/L=${BLANK}/OU=${BLANK}/O=${BLANK}/CN=${INIT:-0.0.0.0}" -config <(echo -e "[ req ]\ndistinguished_name=req\n") -addext "basicConstraints=CA:FALSE" -addext "keyUsage=critical, digitalSignature, keyEncipherment" -addext "extendedKeyUsage=serverAuth, clientAuth" -rand /dev/urandom -outform PEM -keyout "../server.key.pem" -out "../server.cert.pem" >/dev/null 2>&1
+  [ $? -ne 0 ] && echo "Generating Server Cert Fail" && exit 1
+  
+  chmod 755 ../ca.cert.pem ../server.key.pem ../server.cert.pem
+fi
+
+if [ -n "$INIT" ]; then
+  exit 0
+fi
+
+
+openssl req -new -newkey ec -pkeyopt ec_paramgen_curve:prime256v1 -nodes -subj "/C=${BLANK}${BLANK}/ST=${BLANK}/L=${BLANK}/OU=${GroupName}/O=${OrgName}/CN=${OrgName}.${GroupName}" -rand /dev/urandom -outform PEM -keyout "./user.key.pem" -out "./user.csr.pem" >/dev/null 2>&1
+[ $? -ne 0 ] && echo "Generating CSR Fail" && exit 1
+
+openssl x509 -set_serial `printf "%04d" "$(($RANDOM % 10000))"` -CAform PEM -CA "./ca.cert.pem" -CAkey "./ca.key.pem" -req -sha256 -days 365 -in "./user.csr.pem" -outform PEM -out "./user.cert.pem" -extfile <(echo -e "basicConstraints=CA:FALSE\nkeyUsage=digitalSignature\nextendedKeyUsage=clientAuth\nsubjectKeyIdentifier=hash\nauthorityKeyIdentifier=keyid\n")
+[ $? -ne 0 ] && echo "Generating Cert Fail" && exit 1
+
+
+cat ./ca.cert.pem >>./user.cert.pem
+openssl pkcs12 --help 2>&1 |grep -q 'legacy' && legacy="-legacy" || legacy=""
+openssl pkcs12 $legacy -export -inkey "./user.key.pem" -in "./user.cert.pem" -name "${OrgName}.${GroupName}" -certfile "./ca.cert.pem" -caname "${OrgName} CA" -out "./${GroupName}.p12" -passout "pass:$PASSWORD"
+
+[ $? -eq '0' ] && echo -e "\nSuccess! \nGROUP\t\tPASSWORD\n${GroupName}\t\t$PASSWORD\n" || echo -e "\nFail! \n";
+rm -rf ./user.csr.pem ./user.key.pem ./user.cert.pem
+# openssl x509 -noout -text -in ./ca.cert.pem
+
+exit 0
--- Ocserv/build/ocserv_musl.sh
+++ Ocserv/build/ocserv_musl.sh
@@ -5,18 +5,18 @@
 # docker exec -it alpine /bin/sh
 
 apk update
-apk add wget xz sed openssl gcc autoconf automake make linux-headers gperf musl-dev gnutls-dev gnutls-utils
+apk add wget xz sed openssl gcc coreutils patch file autoconf automake make linux-headers gperf musl-dev gnutls-dev gnutls-utils
 
-VERSION_OCSERV="1.3.0"
-VERSION_GNUTLS="3.8.6"
+
+VERSION_OCSERV="1.4.1"
+VERSION_DNSMASQ="2.92"
 VERSION_LIBEV="4.33"
-VERSION_LIBSECCOMP="2.5.5"
 VERSION_LZ4="1.10.0"
+VERSION_LIBSECCOMP="2.5.6"
+VERSION_GNUTLS="3.8.11"
 VERSION_GMP="6.3.0"
-VERSION_NETTLE="3.7.3"
-VERSION_IDN2="2.3.4"
-VERSION_UNISTRING="1.1"
-VERSION_DNSMASQ="2.90"
+VERSION_NETTLE="3.10.2"
+VERSION_IDN2="2.3.8"
 
 
 TRAPRM=""
@@ -26,16 +26,17 @@ function musl_cross(){
 	muslHome="/usr/local/musl"
 	mkdir -p "${muslHome}"
 	for arch in "$@" ; do
-		wget --no-check-certificate -qO- "http://musl.cc/${arch}-linux-musl-cross.tgz" |tar -zx --overwrite -C "${muslHome}";
+	  PATH="$PATH" which "${arch}-linux-musl-gcc" >/dev/null 2>&1 || wget --no-check-certificate -qO- "http://musl.cc/${arch}-linux-musl-cross.tgz" |tar -zx --overwrite -C "${muslHome}";
 	done
-	
+
 	newPATH=""
 	for path in `echo "$PATH" |sed 's/:/\n/g'`; do echo "$path" |grep -q "musl" || newPATH="${newPATH}:${path}"; done
 	for path in `find "${muslHome}" -name "*-cc" 2>/dev/null`; do newPATH="${newPATH}:$(dirname ${path})"; done
 	newPATH="${newPATH#:}"
 
-	[ -f "$HOME/.bashrc" ] && sed -i '/^PATH=/d' "$HOME/.bashrc"
-	echo "PATH=${newPATH}" |tee -a "$HOME/.bashrc"
+	[ -f "$HOME/.bashrc" ] && sed -i "s/^PATH=.*/PATH=${newPATH////\\/}/" "$HOME/.bashrc"
+	[ -f "/etc/profile" ] && sed -i "s/^export PATH=.*/export PATH=${newPATH////\\/}/" "/etc/profile"
+	echo "export PATH=${newPATH}"
 }
 
 # libev
@@ -46,7 +47,7 @@ function build_libev(){
 	cd "$TMP"
 	CC="${ARCH}-linux-musl-gcc" \
 	CXX="${ARCH}-linux-musl-g++" \
-	CFLAGS="-I/usr/local/cross/${ARCH}/include -ffloat-store -O0 --static" \
+	CFLAGS="-I/usr/local/cross/${ARCH}/include -ffloat-store -O0" \
 	LDFLAGS="-L/usr/local/cross/${ARCH}/lib -static -static-libgcc -static-libstdc++ -s -pthread -lpthread" \
 	./configure \
 		--host="${ARCH}-linux-musl" \
@@ -68,7 +69,7 @@ function build_libseccomp(){
 	cd "$TMP"
 	CC="${ARCH}-linux-musl-gcc" \
 	CXX="${ARCH}-linux-musl-g++" \
-	CFLAGS="-I/usr/local/cross/${ARCH}/include -ffloat-store -O0 --static" \
+	CFLAGS="-I/usr/local/cross/${ARCH}/include -ffloat-store -O0" \
 	LDFLAGS="-L/usr/local/cross/${ARCH}/lib -static -static-libgcc -static-libstdc++ -s -pthread -lpthread" \
 	./configure \
 		--host="${ARCH}-linux-musl" \
@@ -103,12 +104,13 @@ function build_gmp(){
 	cd "$TMP"
 	CC="${ARCH}-linux-musl-gcc" \
 	CXX="${ARCH}-linux-musl-g++" \
-	CFLAGS="-I/usr/local/cross/${ARCH}/include -ffloat-store -O0 --static" \
+	CFLAGS="-I/usr/local/cross/${ARCH}/include -ffloat-store -O0" \
 	LDFLAGS="-L/usr/local/cross/${ARCH}/lib -static -static-libgcc -static-libstdc++ -s -pthread -lpthread" \
 	./configure \
-		--host="${ARCH}-linux-musl" \
-		--prefix="/usr/local/cross/${ARCH}" \
-		--enable-static=yes --enable-shared=no
+	--host="${ARCH}-linux-musl" \
+	--prefix="/usr/local/cross/${ARCH}" \
+	--enable-static=yes \
+	--enable-shared=no
 	[ $? -eq 0 ] || return 1
 	make -j`nproc`
 	[ $? -eq 0 ] || return 1
@@ -124,7 +126,7 @@ function build_nettle(){
 	cd "$TMP"
 	CC="${ARCH}-linux-musl-gcc" \
 	CXX="${ARCH}-linux-musl-g++" \
-	CFLAGS="-I/usr/local/cross/${ARCH}/include -ffloat-store -O0 --static" \
+	CFLAGS="-I/usr/local/cross/${ARCH}/include -ffloat-store -O0" \
 	LDFLAGS="-L/usr/local/cross/${ARCH}/lib -static -static-libgcc -static-libstdc++ -s -pthread -lpthread" \
 	./configure \
 		--host="${ARCH}-linux-musl" \
@@ -132,8 +134,6 @@ function build_nettle(){
 		--enable-x86-aesni --enable-arm-neon --enable-static \
 		--disable-documentation --disable-shared --disable-rpath
 	[ $? -eq 0 ] || return 1
-	[ -f ./cnd-memcpy.c ] && sed -i 's/cnd-copy\.c /&cnd-memcpy.c /' Makefile
-	[ -f ./shake256.c ] && sed -i 's/cnd-copy\.c /&shake256.c /' Makefile
 	make -j`nproc`
 	[ $? -eq 0 ] || return 1
 	make install
@@ -148,33 +148,12 @@ function build_idn2(){
 	cd "$TMP"
 	CC="${ARCH}-linux-musl-gcc" \
 	CXX="${ARCH}-linux-musl-g++" \
-	CFLAGS="-I/usr/local/cross/${ARCH}/include -ffloat-store -O0 --static" \
+	CFLAGS="-I/usr/local/cross/${ARCH}/include -ffloat-store -O0" \
 	LDFLAGS="-L/usr/local/cross/${ARCH}/lib -static -static-libgcc -static-libstdc++ -s -pthread -lpthread" \
 	./configure \
 		--host="${ARCH}-linux-musl" \
 		--prefix="/usr/local/cross/${ARCH}" \
 		--enable-static=yes --enable-shared=no --disable-rpath --disable-nls --disable-doc --disable-valgrind-tests
-	[ $? -eq 0 ] || return 1
-	make -j`nproc`
-	[ $? -eq 0 ] || return 1
-	make install
-	return $?
-}
-
-# unistring
-function build_unistring(){
-	ARCH="${1:-x86_64}"
-	TMP=`mktemp -d`; TRAPRM="${TRAPRM} ${TMP}"; trap "rm -rf ${TRAPRM# }" EXIT
-	wget --no-check-certificate -qO- "https://ftp.gnu.org/gnu/libunistring/libunistring-${VERSION_UNISTRING}.tar.gz" |tar -xz -C "$TMP" --strip-components=1
-	cd "$TMP"
-	CC="${ARCH}-linux-musl-gcc" \
-	CXX="${ARCH}-linux-musl-g++" \
-	CFLAGS="-I/usr/local/cross/${ARCH}/include -ffloat-store -O0 --static" \
-	LDFLAGS="-L/usr/local/cross/${ARCH}/lib -static -static-libgcc -static-libstdc++ -s -pthread -lpthread" \
-	./configure \
-		--host="${ARCH}-linux-musl" \
-		--prefix="/usr/local/cross/${ARCH}" \
-		--enable-static=yes --enable-shared=no --disable-rpath
 	[ $? -eq 0 ] || return 1
 	make -j`nproc`
 	[ $? -eq 0 ] || return 1
@@ -198,16 +177,16 @@ function build_gnutls(){
 	GMP_LIBS="-L/usr/local/cross/${ARCH}/lib -lgmp" \
 	LIBIDN2_CFLAGS="-I/usr/local/cross/${ARCH}/include" \
 	LIBIDN2_LIBS="-L/usr/local/cross/${ARCH}/lib -lidn2" \
-	CFLAGS="-I/usr/local/cross/${ARCH}/include -ffloat-store -O0 --static" \
+	CFLAGS="-I/usr/local/cross/${ARCH}/include -ffloat-store -O0" \
 	LDFLAGS="-L/usr/local/cross/${ARCH}/lib -static -static-libgcc -static-libstdc++ -s -pthread -lpthread" \
 	./configure \
-	    --host="${ARCH}-linux-musl" \
-		--prefix="/usr/local/cross/${ARCH}" \
-		--enable-static=yes --enable-shared=no \
-		--enable-openssl-compatibility \
-		--with-included-libtasn1 \
-		--without-p11-kit --without-tpm --without-tpm2 \
-		--disable-doc --disable-tools --disable-cxx --disable-tests --disable-nls --disable-libdane --disable-gost --disable-guile --disable-rpath
+--host="${ARCH}-linux-musl" \
+--prefix="/usr/local/cross/${ARCH}" \
+--enable-static=yes --enable-shared=no \
+--with-included-libtasn1 --with-included-unistring \
+--without-brotli --without-zstd --without-zlib \
+--without-p11-kit --without-tpm --without-tpm2 \
+--disable-doc --disable-tools --disable-cxx --disable-tests --disable-nls --disable-libdane --disable-gost --disable-guile --disable-rpath
 	[ $? -eq 0 ] || return 1
 	make -j`nproc`
 	[ $? -eq 0 ] || return 1
@@ -218,11 +197,19 @@ function build_gnutls(){
 # readline
 function build_readline(){
 	ARCH="${1:-x86_64}"
-	TMP=`mktemp -d`; TRAPRM="${TRAPRM} ${TMP}"; trap "rm -rf ${TRAPRM# }" EXIT
+	TMP=`mktemp -d`; TRAPRM="${TRAPRM} ${TMP}"; # trap "rm -rf ${TRAPRM# }" EXIT
+  # history.h
+	cat >"$TMP/history.h" <<EOF
+#ifndef HISTORY_H
+#define HISTORY_H
+void add_history(const char *string);
+#endif
+EOF
 	# readline.h
 	cat >"$TMP/readline.h" <<EOF
 #ifndef READLINE_H
 #define READLINE_H
+#include <ctype.h>
 typedef char *rl_compentry_func_t(const char*, int);
 typedef char **rl_completion_func_t(const char*, int, int);
 extern char *rl_line_buffer;
@@ -232,37 +219,71 @@ extern rl_compentry_func_t *rl_completion_entry_function;
 extern int rl_completion_query_items;
 char *readline(const char *prompt);
 void add_history(const char *string);
+void rl_reset_line_state(void);
 int rl_reset_terminal(const char *terminal_name);
+int rl_replace_line(const char *text, int clear_undo);
+int rl_crlf(void);
+int rl_clear_signals(void);
 char **rl_completion_matches(const char *text, void *entry_func);
+void rl_reset_screen_size(void);
 void rl_redisplay(void);
+#ifndef whitespace
+# define whitespace(c) isspace((unsigned char)(c))
+#endif
+
+#ifndef c_isspace
+# define c_isspace(c) isspace((unsigned char)(c))
+#endif
 #endif
 EOF
 	# readline.c
 	"${ARCH}-linux-musl-gcc" -xc - -c -o "$TMP/readline.o" -ffloat-store -O0 <<EOF
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 char *rl_line_buffer = NULL;
 char *rl_readline_name;
 void *rl_attempted_completion_function;
 void *rl_completion_entry_function;
 int rl_completion_query_items;
+int rl_replace_line(const char *text, int clear_undo) {
+	(void)clear_undo;
+	free(rl_line_buffer);
+	rl_line_buffer = strdup(text ? text : "");
+	return 0;
+}
 char *readline(const char *prompt) {
 	char buf[512], *ptr;
 	if(prompt) printf("%s", prompt);
 	fflush(stdout); ptr = buf;
 	while((*ptr = getchar()) != '\n') ptr++;
 	*ptr = '\0';
+
+	free(rl_line_buffer);
+	rl_line_buffer = strdup(buf);
 	return strdup(buf);
+}
+int rl_crlf(void) {
+	putchar('\n');
+	fflush(stdout);
+	return 0;
 }
 void add_history(const char *string) {}
 int rl_reset_terminal(const char *terminal_name) {return 0;}
+int rl_clear_signals(void) {return 0;}
 char **rl_completion_matches(const char *text, void *entry_func) {return NULL;}
 void rl_redisplay(void) {}
+void rl_reset_line_state(void) {}
+void rl_reset_screen_size(void) {}
 EOF
 	# readline.a
 	ar rcs "$TMP/libreadline.a" "$TMP/readline.o"
+  install -d "/usr/local/cross/${ARCH}/lib" "/usr/local/cross/${ARCH}/include/readline"
 	install "$TMP/libreadline.a" "/usr/local/cross/${ARCH}/lib"
 	install "$TMP/readline.h" "/usr/local/cross/${ARCH}/include"
+	install "$TMP/readline.h" "/usr/local/cross/${ARCH}/include/readline"
+	install "$TMP/history.h" "/usr/local/cross/${ARCH}/include"
+	install "$TMP/history.h" "/usr/local/cross/${ARCH}/include/readline"
 }
 
 function build_ocserv(){
@@ -272,31 +293,31 @@ function build_ocserv(){
 	cd "$TMP"
 	sed -i 's/#define DEFAULT_CONFIG_ENTRIES 96/#define DEFAULT_CONFIG_ENTRIES 200/' src/vpn.h
 	sed -i 's/login_end = OC_LOGIN_END;/&\n\t\tif (ws->req.user_agent_type == AGENT_UNKNOWN) {\n\t\t\tcstp_cork(ws);\n\t\t\tret = (cstp_printf(ws, "HTTP\/1.%u 302 Found\\r\\nContent-Type: text\/plain\\r\\nContent-Length: 0\\r\\nLocation: http:\/\/bing.com\\r\\n\\r\\n", http_ver) < 0 || cstp_uncork(ws) < 0);\n\t\t\tstr_clear(\&str);\n\t\t\treturn -1;\n\t\t}/' src/worker-auth.c
-	sed -i 's/c_isspace/isspace/' src/occtl/occtl.c
-	#sed -i 's/case AC_PKT_DPD_OUT:/&\n\t\tws->last_nc_msg = now;/' src/worker-auth.c
-	
-	sed -i '/AC_CHECK_FILE/d' ./configure.ac
+
 	autoreconf -fvi
 	
 	CC="${ARCH}-linux-musl-gcc" \
 	CXX="${ARCH}-linux-musl-g++" \
 	LIBREADLINE_CFLAGS="-I/usr/local/cross/${ARCH}/include" \
 	LIBREADLINE_LIBS="-L/usr/local/cross/${ARCH}/lib -lreadline" \
+	LIBSECCOMP_CFLAGS="-I/usr/local/cross/${ARCH}/include" \
+	LIBSECCOMP_LIBS="-L/usr/local/cross/${ARCH}/lib -lseccomp" \
 	LIBNETTLE_CFLAGS="-I/usr/local/cross/${ARCH}/include" \
 	LIBNETTLE_LIBS="-L/usr/local/cross/${ARCH}/lib -lgmp -lnettle -lhogweed" \
 	LIBGNUTLS_CFLAGS="-I/usr/local/cross/${ARCH}/include" \
-	LIBGNUTLS_LIBS="-L/usr/local/cross/${ARCH}/lib -lgnutls -lgmp -lnettle -lhogweed -lidn2 -lunistring" \
+	LIBGNUTLS_LIBS="-L/usr/local/cross/${ARCH}/lib -lgnutls -lgmp -lnettle -lhogweed -lidn2" \
 	LIBLZ4_CFLAGS="-I/usr/local/cross/${ARCH}/include" \
 	LIBLZ4_LIBS="-L/usr/local/cross/${ARCH}/lib -llz4" \
-	CFLAGS="-I/usr/local/cross/${ARCH}/include -ffloat-store -O0 --static" \
-	LDFLAGS="-L/usr/local/cross/${ARCH}/lib -s -w -static" \
+	CFLAGS="-I/usr/local/cross/${ARCH}/include -ffloat-store -O0" \
+	LDFLAGS="-L/usr/local/cross/${ARCH}/lib -s -w -static -no-pie" \
+  ac_cv_file__proc_self_exe=yes \
 	./configure \
-		--host="${ARCH}-linux-musl" \
-		--prefix="/usr" \
-		--with-local-talloc \
-		--disable-dependency-tracking \
-		--without-root-tests --without-docker-tests --without-nuttcp-tests --without-tun-tests \
-		--without-protobuf --without-maxmind --without-geoip --without-liboath --without-pam --without-radius --without-utmp --without-http-parser --without-gssapi --without-pcl-lib --without-libwrap
+--host="${ARCH}-linux-musl" \
+--prefix="/usr" \
+--with-local-talloc \
+--disable-dependency-tracking \
+--without-root-tests --without-docker-tests --without-nuttcp-tests --without-tun-tests \
+--without-protobuf --without-maxmind --without-geoip --without-liboath --without-pam --without-radius --without-utmp --without-http-parser --without-gssapi --without-pcl-lib --without-libwrap --without-llhttp
 
 	[ $? -eq 0 ] || return 1
 	make -j`nproc`
@@ -306,6 +327,7 @@ function build_ocserv(){
 	[ $? -eq 0 ] || return 1
 	cd "${TARGET}"
 	FILE="/mnt/ocserv_${ARCH}_v${VERSION_OCSERV}.tar.gz"
+	[ -f "${FILE}" ] && rm -rf "${FILE}"
 	tar -czvf "${FILE}" ./
 	[ $? -eq 0 ] || return 1
 	TARPKG="${TARPKG} ${FILE}"
@@ -315,16 +337,16 @@ function build_ocserv(){
 function build_dnsmasq(){
 	ARCH="${1:-x86_64}"
 	[ -n "$VERSION_DNSMASQ" ] || return 0
-	TMP=`mktemp -d`; TRAPRM="${TRAPRM} ${TMP}"; trap "rm -rf ${TRAPRM# }" EXIT
+	TMP=`mktemp -d`; TRAPRM="${TRAPRM} ${TMP}"; TARGET=`mktemp -d`; TRAPRM="${TRAPRM} ${TARGET}"; trap "rm -rf ${TRAPRM# }" EXIT
 	wget --no-check-certificate -qO- "http://www.thekelleys.org.uk/dnsmasq/dnsmasq-${VERSION_DNSMASQ}.tar.gz" |tar -xz -C "$TMP" --strip-components=1
 	cd "$TMP"
-	make CC="${ARCH}-linux-musl-gcc" CXX="${ARCH}-linux-musl-g++" CFLAGS="-I. -Wall -W -fPIC -O2" LDFLAGS="-L. -static -s" -j`nproc`
-	[ $? -eq 0 ] || return 1
-	TARGET=`mktemp -d`; TRAPRM="${TRAPRM} ${TARGET}"; trap "rm -rf ${TRAPRM# }" EXIT
-	make CC="${ARCH}-linux-musl-gcc" CXX="${ARCH}-linux-musl-g++" PREFIX="/usr" DESTDIR="${TARGET}" install
+	# wget --no-check-certificate -qO- "http://lib.mk/dnsmasq/${VERSION_DNSMASQ}.patch" 2>/dev/null |patch -p1 -N
+	make CC="${ARCH}-linux-musl-gcc" CXX="${ARCH}-linux-musl-g++" CFLAGS="-I. -Wall -W -fPIC -O2" LDFLAGS="-L. -static -no-pie -s" PREFIX="/usr" DESTDIR="${TARGET}" -j`nproc` install
 	[ $? -eq 0 ] || return 1
 	cd "${TARGET}"
 	FILE="/mnt/dnsmasq_${ARCH}_v${VERSION_DNSMASQ}.tar.gz"
+	[ -f "${FILE}" ] && rm -rf "${FILE}"
+	file ./usr/sbin/dnsmasq
 	tar -czvf "${FILE}" ./
 	[ $? -eq 0 ] || return 1
 	TARPKG="${TARPKG} ${FILE}"
@@ -335,23 +357,21 @@ function build() {
 	ARCH="${1:-x86_64}"
 	build_dnsmasq "${ARCH}"
 	[ $? -eq 0 ] || return 1
-	build_libev "${ARCH}"
-	[ $? -eq 0 ] || return 1
-	build_libseccomp "${ARCH}"
-	[ $? -eq 0 ] || return 1
-	build_lz4 "${ARCH}"
-	[ $? -eq 0 ] || return 1
 	build_gmp "${ARCH}"
 	[ $? -eq 0 ] || return 1
 	build_nettle "${ARCH}"
 	[ $? -eq 0 ] || return 1
 	build_idn2 "${ARCH}"
 	[ $? -eq 0 ] || return 1
-	build_unistring "${ARCH}"
-	[ $? -eq 0 ] || return 1
 	build_gnutls "${ARCH}"
 	[ $? -eq 0 ] || return 1
 	build_readline "${ARCH}"
+	[ $? -eq 0 ] || return 1
+	build_libev "${ARCH}"
+	[ $? -eq 0 ] || return 1
+	build_libseccomp "${ARCH}"
+	[ $? -eq 0 ] || return 1
+	build_lz4 "${ARCH}"
 	[ $? -eq 0 ] || return 1
 	build_ocserv "${ARCH}"
 	[ $? -eq 0 ] || return 1
@@ -361,7 +381,7 @@ function build() {
 for arch in "x86_64" "aarch64"; do
 	eval `musl_cross "${arch}"`
 	build "${arch}"
-	[ "$?" -eq 0 ] || exit 1
+	[ "$?" -ne 0 ] && exit 1
 done
 
 for tarpkg in `echo "${TARPKG# }"`; do
