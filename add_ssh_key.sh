1bb84469fd363ff0c7d762b1ca7d47f9630f3bc9
Jinpeng Peng <35778015+ixmu@users.noreply.github.com>
2026-06-26 22:01:17 +0800

    Update add_ssh_key.sh

--- add_ssh_key.sh
+++ add_ssh_key.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # curl -sSL https://raw.githubusercontent.com/ixmu/Note/master/add_ssh_key.sh |bash
 # SSH 公钥内容
-ssh_public_key="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDIVVbZkwvMkPxQJsWdQYulS3iFn+tJaWDRw6CTD0i6NZWpfOY+biZlOvwJq4qcEAn5tAHcxXEBhnTtEc/+0y+YvjRx20OBsl6Z2bSabuZ3jGZ1q812mNXAA5f+6S1hHoPgrRDpYhQr2FW6du3wxZDpcgjxoljFYIRpSIHvixlJa1TTKrDLDkRZG4sdjz6K+Cat59qP99dyfU2ym2pH6UPaqSwtGT36VDPo61zB/dxjeYalKmqQGa70SqxJsEn+/DSEht8c/hdRNlE0UpagRxrjTtZ446C5sBlF7uEUPdbAV0EdRXRlhGRYJcKc+VOJvnROTvwJztLf6NGFh2n9H6hB root@iZwz9avyv2945ilt432lffZ"
+ssh_public_key="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDIVVbZkwvMkPxQJsWdQYulS3iFn+tJaWDRw6CTD0i6NZWpfOY+biZlOvwJq4qcEAn5tAHcxXEBhnTtEc/+0y+YvjRx20OBsl6Z2bSabuZ3jGZ1q812mNXAA5f+6S1hHoPgrRDpYhQr2FW6du3wxZDpcgjxoljFYIRpSIHvixlJa1TTKrDLDkRZG4sdjz6K+Cat59qP99dyfU2ym2pH6UPaqSwtGT36VDPo61zB/dxjeYalKmqQGa70SqxJsEn+/DSEht8c/hdRNlE0UpagRxrjTtZ446C5sBlF7uEUPdbAV0EdRXRlhGRYJcKc+VOJvnROTvwJztLf6NGFh2n9H6hB root@iZwz9avyv2945ilt432lffZ \rssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGb5qLL+txob4mVgaEq6fR1xXQQG3KT81cJpuYrVnO3w pengxp1996@hotmail.com
+"
 
 # 将 SSH 公钥添加到授权密钥文件
 mkdir -p ~/.ssh && echo "${ssh_public_key}" >> ~/.ssh/authorized_keys
